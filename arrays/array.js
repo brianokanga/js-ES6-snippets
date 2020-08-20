a2ddac182f31298c5886511506b92dd887f000e7
Brian Okanga <briannokanga@gmail.com>
2020-08-20 04:57:43 +0300

    Added reviews for arrays and arrow functions

--- arrays/array.js
+++ arrays/array.js
@@ -22,20 +22,64 @@
 
 // console.log(myArray);
 
-var years = [1960, 1978, 1895, 1995, 1700];
+// var years = [1960, 1978, 1895, 1995, 1700];
 
-function arrCalc(arr, fn) {
-	var arrAges = [];
-	for (var i = 0; i < arr.length; i++) {
-		arrAges.push(fn(arr[i]));
-	}
-	return arrAges;
-}
+// function arrCalc(arr, fn) {
+// 	var arrAges = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		arrAges.push(fn(arr[i]));
+// 	}
+// 	return arrAges;
+// }
 
-function calcAge(el) {
-	return 2020 - el;
-}
+// function calcAge(el) {
+// 	return 2020 - el;
+// }
 
-var x = arrCalc(years, calcAge);
+// var x = arrCalc(years, calcAge);
 
-console.log(x);
+// console.log(x);
+
+
+// const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+// const map1 = array1.map(x => x * 2);
+
+
+
+// expected output: Array [2, 8, 18, 32]
+
+// let calculateTotal = function (type) {
+// 	let sum = 0;
+// 	data.allItems[type].forEach(function (cur) {
+// 		sum += cur.value;
+// 	});
+// 	data.totals[type] = sum;
+// };
+
+
+// var array1 = [1, 4, 9, 16];
+
+// var map1 = array1.forEach(function (cur) {
+// 	newFunction(cur);
+// });
+
+// function newFunction(cur) {
+// 	cur * 2;
+// }
+
+// console.log(map1);
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+let results =  numbers.map(function(cur) {
+	return cur * 3;
+});
+
+const results6 = numbers.map(cur => cur * 3);
+
+console.log(results);
+console.log(results6);
+
--- arrow functions/app.js
+++ arrow functions/app.js
@@ -2,76 +2,90 @@
 const years = [1990, 1965, 1983, 1937];
 
 //in es5
-var ages = years.map (function (el) {
-  return 2016 - el;
+var ages5 = years.map(function (el) {
+	return 2016 - el;
 });
 
 //in ES6
-let ages6 = years.map (el => 2016 - el);
+let ages6 = years.map(el => 2016 - el);
 
-console.log (ages);
-console.log (ages6);
+console.log(ages5);
+console.log(ages6);
 
 //where the is two arguments
-ages6 = years.map ((el, index) => `age element ${index + 1}: ${2016 - el}`);
+ages6 = years.map((el, index) => `age element ${index + 1}: ${2016 - el}`);
 
-console.log (ages6);
+console.log(ages6);
 
 //if we have more than one line
-ages6 = years.map ((el, index) => {
-  const now = new Date ().getFullYear ();
-  const age = now - el;
-  return `age element ${index + 1}: ${age}`;
+ages6 = years.map((el, index) => {
+	const now = new Date().getFullYear();
+	const age = now - el;
+	return `age element ${index + 1}: ${age}`;
 });
 
-console.log (ages6);
+console.log(ages6);
 
+console.log('========LEXICAL THIS FOR ARROW FUNCTIONS=============');
+
+//LEXICAL THIS FOR ARROW FUNCTIONS
 //Arrow functions do not have their own this Keyword hence use the this keyword of the parent element
 
 //lexical  "this"
 //this word work
-const box6 = {
-  color: 'green',
-  position: 1,
-  // this is a regular function call hence
-  clickme: function () {
-    // the second function is a regular function call(callback)
-    var self = this; //share the this keyword with te click me function which has access the object this
-    document.querySelector ('.green').addEventListener ('click', function () {
-      var str = `this is box number ${this.position} and it is ${this.color} `;
-      alert (str);
-    });
-  },
+var box5 = {
+	color: 'green',
+	position: 1,
+	// this is a regular function call hence
+	clickme: function () {
+		// the second function is a regular function call(callback)
+		var self = this; //share the this keyword with te click me function which has access the object this
+		document.querySelector('.green').addEventListener('click', function () {
+			var str = `this is box number ${self.position} and it is ${self.color} `;
+			alert(str);
+		});
+	},
 };
-box6.clickme ();
+box5.clickme();
 
-console.log ('=================================================');
+//ES6
+var box6 = {
+	color: 'green',
+	position: 1,
+	clickme: function () {
+		document.querySelector('.green').addEventListener('click', () => {
+			var str = `this is box number ${this.position} and it is ${this.color} `;
+			alert(str);
+		});
+	},
+};
+box6.clickme();
 
-function Person (name) {
-  this.name = name;
+console.log('=================================================');
+
+function Person(name) {
+	this.name = name;
 }
-
 // ES5
 Person.prototype.myFriends5 = function (friends) {
-  var arr = friends.map (
-    function (el) {
-      return this.name + ' is friends with ' + el;
-    }.bind (this)
-  );
+	var arr = friends.map(
+		function (el) {
+			return this.name + ' is friends with ' + el;
+		}.bind(this)
+	);
 
-  console.log (arr);
+	console.log(arr);
 };
 
-var friends = ['Bob', 'Jane', 'Mark'];
-new Person ('John').myFriends5 (friends);
+var friends = ['Bob', 'Jane', 'Mark']; //this array is hoisted to the top during execution
+new Person('John').myFriends5(friends);
 
-console.log ('=================================================');
+console.log('=============ENTERING ES6 VERSION ==================');
 
 // ES6
 Person.prototype.myFriends6 = function (friends) {
-  var arr = friends.map (el => `${this.name} is friends with ${el}`);
-
-  console.log (arr);
+	var arr = friends.map(el => `${this.name} is friends with ${el}`);
+	console.log(arr);
 };
 
-new Person ('Mike').myFriends6 (friends);
+new Person('Mike').myFriends6(friends);
