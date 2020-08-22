b2ad18ff0dfeb95326dcf2387575acdb619fe070
Brian Okanga <briannokanga@gmail.com>
2020-08-23 01:17:22 +0300

    Added the sprad operator

--- arrays/arraysEs6.js
+++ arrays/arraysEs6.js
@@ -15,40 +15,52 @@
 // }
 
 //ES6
-const boxes = document.querySelectorAll('.box');
-const boxesArr6 = Array.from(boxes);
-boxesArr6.forEach(cur => (cur.style.backgroundColor = 'dodgerblue'));
+const boxes = document.querySelectorAll ('.box');
+const boxesArr6 = Array.from (boxes);
+boxesArr6.forEach (cur => (cur.style.backgroundColor = 'dodgerblue'));
 for (const cur of boxesArr6) {
-	if (cur.className.includes('blue')) {
-		continue; //skips the if true
-	}
-	cur.textContent = 'changed to blue';
+  if (cur.className.includes ('blue')) {
+    continue; //skips the if true
+  }
+  cur.textContent = 'changed to blue';
 }
 
 //findIndex work like map/forEach has access to current, index and entire array
 // find the child over 18 and their age
 let ages = [12, 17, 8, 21, 14, 11];
-let index = ages.findIndex(cur => cur >= 18);
-let over18 = ages.find(cur => cur >= 18);
+let index = ages.findIndex (cur => cur >= 18);
+let over18 = ages.find (cur => cur >= 18);
 
-console.log(index);
-console.log(over18);
+console.log (index);
+console.log (over18);
 
 //SPREAD OPERATOR
 //works the same as apply()
-function addFourAges(a, b, c, d) {
-	return a + b + c + d;
+function addFourAges (a, b, c, d) {
+  return a + b + c + d;
 }
 
-let ageset = [18, 30, 12, 21];
-const sum = addFourAges(...ageset);
-console.log(sum);
+// ES5 version using apply method
+var ageset5 = [18, 30, 12, 21];
+var sum5 = addFourAges.apply (null, ageset5);
+
+console.log (sum5);
+
+//SPREAD OPERATOR
+//works the same as apply()
+function addFourAges (a, b, c, d) {
+  return a + b + c + d;
+}
+
+let ageset6 = [18, 30, 12, 21];
+const sum6 = addFourAges (...ageset6);
+console.log (sum6);
 
 //SPREAD OPERATOR for joining Arrays
 const familySmith = ['John', 'Jane', 'Mark'];
 const familyMiller = ['Mary', 'Bob', 'Ann'];
 const bigFamily = [...familySmith, 'Eliza', ...familyMiller];
-console.log(bigFamily);
+console.log (bigFamily);
 
 //SPREAD OPERATOR on nodelist
 
