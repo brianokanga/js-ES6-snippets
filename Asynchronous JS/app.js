70a58d4e90941dfe1b17e49664575dc95ddf5dc7
Brian Okanga <briannokanga@gmail.com>
2020-08-24 02:02:29 +0300

    Added OLD WAYS ASYNCHRONOUS WITH CALLBACKS: CALLBACK HELL Example

--- Asynchronous JS/app.js
+++ Asynchronous JS/app.js
@@ -1,14 +1,44 @@
 //ASYNCHRONOUS JS
+//simple asynchronous example
+// const second = () => {
+// 	setTimeout(() => {
+// 		console.log('Async, Are we in yet?');
+// 	}, 2000);
+// };
+// const first = () => {
+// 	console.log('Are we in yet?');
+// 	second();
+// 	console.log('The End');
+// };
 
-const second = () => {
+// first();
+
+//OLD WAYS ASYNCHRONOUS WOTH CALLBACKS
+
+//fake recipe reader simulating ajax calls loading data from remote server
+function getRecipe() {
 	setTimeout(() => {
-		console.log('Async, Are we in yet?');
-	}, 2000);
-};
-const first = () => {
-	console.log('Are we in yet?');
-	second();
-	console.log('The End');
-};
+		const recipeID = [523, 883, 432, 974];
+		console.log(recipeID);
 
-first();
+		//get recipe for one of the ids
+		setTimeout(
+			id => {
+				const recipe = { title: 'Fresh tomato pasta', publisher: 'Brian' };
+				console.log(`${id}: ${recipe.title}`);
+
+				setTimeout(
+					publisher => {
+						const recipe2 = { title: 'Italian Pizza', publisher: 'Brian' };
+						console.log(recipe);
+					},
+					1500,
+					recipe.publisher
+				);
+			},
+			1500,
+			recipeID[2]
+		);
+	}, 1500);
+}
+getRecipe();
