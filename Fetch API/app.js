0294e5f3b3edb47c1e1beb601eefe31d88ac2c8d
Brian Okanga <briannokanga@gmail.com>
2020-08-24 07:24:10 +0300

    Added simple Fetch api example using async await on a  third party api

--- Fetch API/app.js
+++ Fetch API/app.js
@@ -20,3 +20,41 @@ function getWeather(woeid) {
 
 getWeather(44418);
 getWeather(4747);
+
+//async await version
+// async function getWeather(woeid) {
+// 	//promise 1
+// 	const result = await fetch(
+// 		`https://www.metaweather.com/api/location/${woeid}`
+// 	);
+// 	//promise 2
+// 	const data = await result.json(); // returns a promise
+// 	console.log(
+// 		`Temperatures today in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`
+// 	);
+// }
+
+async function getWeatherAW(woeid) {
+	try {
+		const result = await fetch(
+			`https://www.metaweather.com/api/location/${woeid}/`
+		);
+		const data = await result.json();
+		const tomorrow = data.consolidated_weather[1];
+		console.log(
+			`Temperatures tomorrow in ${data.title} stay between ${tomorrow.min_temp} and ${tomorrow.max_temp}.`
+		);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+getWeatherAW(2487956);
+
+//
+let dataLondon;
+getWeatherAW(44418).then(data => {
+	dataLondon = data;
+	console.log(dataLondon);
+});
