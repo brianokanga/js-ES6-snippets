ecc777c462acf4f421bbd85b119805f71701510c
Brian Okanga <briannokanga@gmail.com>
2020-08-29 05:21:45 +0300

    Added classes and subclasses review

--- Classesin es6/clasesAndSubclasses.js
+++ Classesin es6/clasesAndSubclasses.js
@@ -4,77 +4,77 @@
 //es5 verssion using functional constructor
 //Super class
 var Person5 = function (name, yearOfBirth, job) {
-	this.name = name;
-	this.yearOfBirth = yearOfBirth;
-	this.job = job;
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
 };
 
 Person5.prototype.calculateAge = function () {
-	var age = new Date().getFullYear() - this.yearOfBirth;
-	console.log(age);
+  var age = new Date ().getFullYear () - this.yearOfBirth;
+  console.log (age);
 };
 
 //subclass
 var Athlete5 = function (name, yearOfBirth, job, olympicGames, medals) {
-	Person5.call(this, name, yearOfBirth, job);
-	this.olympicGames = olympicGames;
-	this.medals = medals;
+  Person5.call (this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
 };
 
 //inheritance in classes
 //object.create allows us to set prototype of an object
 //we want the prototype of athlete to be the protype of Person so that they become connected
-Athlete5.prototype = Object.create(Person5.prototype);
+Athlete5.prototype = Object.create (Person5.prototype);
 
 //prototype on sub class
 Athlete5.prototype.wonMedal = function () {
-	this.medals++;
-	console.log(this.medals);
+  this.medals++;
+  console.log (this.medals);
 };
 
-var johnAthlete5 = new Athlete5('Jphn', 1990, 'Swimmer', 3, 10);
+var johnAthlete5 = new Athlete5 ('Jphn', 1990, 'Swimmer', 3, 10);
 
-johnAthlete5.calculateAge();
-johnAthlete5.wonMedal();
+johnAthlete5.calculateAge ();
+johnAthlete5.wonMedal ();
 
-console.log('=========ES6 VERSION===========');
+console.log ('=========ES6 VERSION===========');
 
 //ES6 VERSION
 //ES6 inheritance using classes
 
-//super class
+//super class(main class)
 class Person6 {
-	constructor(name, yearOfBirth, job) {
-		this.name = name;
-		this.yearOfBirth = yearOfBirth;
-		this.job = job;
-	}
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
 
-	calculateAge() {
-		let age = new Date().getFullYear() - this.yearOfBirth;
-		console.log(age);
-	}
+  calculateAge () {
+    let age = new Date ().getFullYear () - this.yearOfBirth;
+    console.log (age);
+  }
 
-	//used as helper functions but not inheritable by instances of the class
-	static greeting() {
-		console.log('Hey there');
-	}
+  //used as helper functions but not inheritable by instances of the class
+  static greeting () {
+    console.log ('Hey there');
+  }
 }
 
 class Athlete6 extends Person6 {
-	constructor(name, yearOfBirth, job, olympicGames, medals) {
-		super(name, yearOfBirth, job);
-		this.olympicGames = olympicGames;
-		this.medals = medals;
-	}
+  constructor (name, yearOfBirth, job, olympicGames, medals) {
+    super (name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
 
-	wonMedal() {
-		this.medals++;
-		console.log(this.medals);
-	}
+  wonMedal () {
+    this.medals++;
+    console.log (this.medals);
+  }
 }
 
-const elizaAthlete6 = new Athlete6('Eliza', 1994, 'Swimmer', 5, 13);
+const elizaAthlete6 = new Athlete6 ('Eliza', 1994, 'Swimmer', 5, 13);
 
-elizaAthlete6.calculateAge();
-elizaAthlete6.wonMedal();
+elizaAthlete6.calculateAge ();
+elizaAthlete6.wonMedal ();
--- Classesin es6/classes.js
+++ Classesin es6/classes.js
@@ -2,38 +2,38 @@
 
 //es5 functional constructor
 var Person5 = function (name, yearOfBirth, job) {
-	this.name = name;
-	this.yearOfBirth = yearOfBirth;
-	this.job = job;
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
 };
 
 Person5.prototype.calculateAge = function () {
-	var age = new Date().getFullYear() - this.yearOfBirth;
-	console.log(age);
+  var age = new Date ().getFullYear () - this.yearOfBirth;
+  console.log (age);
 };
 
-var john5 = new Person5('john', 1990, 'teacher');
+var john5 = new Person5 ('john', 1990, 'teacher');
 
 //ES6 classes
 class Person6 {
-	constructor(name, yearOfBirth, job) {
-		this.name = name;
-		this.yearOfBirth = yearOfBirth;
-		this.job = job;
-	}
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
 
-	calculateAge() {
-		let age = new Date().getFullYear() - this.yearOfBirth;
-		console.log(age);
-	}
+  calculateAge () {
+    let age = new Date ().getFullYear () - this.yearOfBirth;
+    console.log (age);
+  }
 
-	//used as helper functions but not inheritable by instances of the class
-	static greeting() {
-		console.log('Hey there');
-	}
+  //used as helper functions but not inheritable by instances of the class
+  static greeting () {
+    console.log ('Hey there');
+  }
 }
 
-const john6 = new Person6('Eliza', 1994, 'Dev');
+const john6 = new Person6 ('Eliza', 1994, 'Dev');
 
-//static method inheritable by instances of the class hence called
-Person6.greeting();
+//static method are not inheritable by instances of the class hence called
+Person6.greeting ();
